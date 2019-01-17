89219dcd089abed4cfed9fd89629e8a14703d8ee
Jirka <jirka.borovec@seznam.cz>
2019-01-17 15:12:37 +0100

    logging w. %r

--- benchmark/utilities/visualisation.py
+++ benchmark/utilities/visualisation.py
@@ -91,11 +91,11 @@ def draw_landmarks_origin_target_warped(ax, points_origin, points_target,
     ...                                    points, points + 1, points - 1)
     """
     assert points_target.shape == points_origin.shape, \
-        'image dimension has to match %s != %s' \
-        % (repr(points_target.shape), repr(points_origin.shape))
+        'image dimension has to match %r != %r' \
+        % (points_target.shape, points_origin.shape)
     assert points_origin.shape == points_warped.shape, \
-        'image dimension has to match %s != %s' \
-        % (repr(points_origin.shape), repr(points_warped.shape))
+        'image dimension has to match %r != %r' \
+        % (points_origin.shape, points_warped.shape)
     ax.plot(points_origin[:, 0], points_origin[:, 1], marker, color='b',
             label='Original positions')
     # draw a dotted line between origin and where it should be
@@ -132,9 +132,9 @@ def overlap_two_images(image1, image2, transparent=0.5):
            [ 0.5,  0.5,  0.5,  0.5,  0.5,  0.1],
            [ 0.4,  0.4,  0.4,  0.4,  0.4,  0. ]])
     """
-    assert image1.ndim == 3, 'required RGB images, got %s' % repr(image1.ndim)
-    assert image1.ndim == image2.ndim, 'image dimension has to match, %s != %s' \
-                                       % (repr(image1.ndim), repr(image2.ndim))
+    assert image1.ndim == 3, 'required RGB images, got %i' % image1.ndim
+    assert image1.ndim == image2.ndim, 'image dimension has to match, %r != %r' \
+                                       % (image1.ndim, image2.ndim)
     size1, size2 = image1.shape, image2.shape
     max_size = np.max(np.array([size1, size2]), axis=0)
     image = np.zeros(max_size)
@@ -186,7 +186,7 @@ def create_figure(im_size, figsize_max=MAX_FIGURE_SIZE):
     >>> isinstance(fig, plt.Figure)
     True
     """
-    assert len(im_size) >= 2, 'not valid image size - %s' % repr(im_size)
+    assert len(im_size) >= 2, 'not valid image size - %r' % im_size
     size = np.array(im_size[:2])
     fig_size = size[::-1] / float(size.max()) * figsize_max
     fig, ax = plt.subplots(figsize=fig_size)
